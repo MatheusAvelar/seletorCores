a30a10cf16b17b6fb9e8eea19f8514bddc33a8d9
Matheus <49567690+MatheusAvelar@users.noreply.github.com>
2023-04-27 16:02:30 -0300

    Melhoria

--- function.js
+++ function.js
@@ -1,9 +1,27 @@
-function geradorCores(){
-    var cor = '';
-    while (cor.length < 6) {
-      cor += (Math.random()).toString(16).substr(-6).substr(-1)
-    }
-    var newColor =  '#' + cor;
-    document.body.style.backgroundColor = newColor;
-    document.querySelector('#color').textContent = newColor;
+const colorInput = document.getElementById("color-input");
+const colorSwatch = document.getElementById("color-swatch");
+const randomColorButton = document.getElementById("random-color-button");
+const colorName = document.getElementById("color-name");
+
+randomColorButton.addEventListener("click", function() {
+  const randomColor = getRandomColor();
+  colorInput.value = randomColor;
+  colorSwatch.style.backgroundColor = randomColor;
+  colorName.innerHTML = randomColor;
+  document.body.style.backgroundColor = randomColor; // Altera o fundo da tela
+});
+
+colorInput.addEventListener("input", function() {
+  colorSwatch.style.backgroundColor = colorInput.value;
+  colorName.innerHTML = colorInput.value;
+  document.body.style.backgroundColor = colorInput.value; // Altera o fundo da tela
+});
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
